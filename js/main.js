245e96d2a61eb2c606b15423bb75bae6f3ac09c0
Amtrix <naomi.yoriji@amtrix.jp>
2023-05-06 19:37:36 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, 800, "swing");
+    return false;
+  });
+})
